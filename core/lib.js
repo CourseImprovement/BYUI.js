1cc6b0a27384b75fee878944f850c7a65af79894
= <chase.willden@gmail.com>
2015-11-16 13:26:43 -0700

    updated

--- core/lib.js
+++ core/lib.js
@@ -6,3 +6,28 @@
  */
 byui.lib = byui.lib || {}
 
+byui.lib._libs = {
+	jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js',
+	angular: 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular.min.js',
+	angularRoute: 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.1/angular-route.min.js',
+	highcharts: 'https://cdnjs.cloudflare.com/ajax/libs/highcharts/4.1.9.1/highcharts.js'
+}
+
+/**
+ * Include a popular library
+ * @param  {String} name Name of the library
+ * @return {[type]}      [description]
+ */
+byui.lib.include = function(name){
+	var url = byui.lib._libs[name];
+	if (url){
+		byui._ready.queue++;
+		byui.http.get(url, function(js){
+			eval(js);
+			byui._ready.loaded++;
+		});
+	}
+	else{
+		byui.error('Library not added, please add');
+	}
+}
