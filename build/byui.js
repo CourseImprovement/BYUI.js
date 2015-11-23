56a1aa1b3926b172985ffde383062f02aa5f92af
= <chase.willden@gmail.com>
2015-11-23 13:55:51 -0700

    fixed endless recursion

--- build/byui.js
+++ build/byui.js
@@ -39,7 +39,7 @@ window.byui = (function(){
 		getType: function(obj){
 			var t = Object.prototype.toString.call(obj);
 			var ty = t.replace('[object ', '').replace(']', '').toLowerCase();
-			if (byui(ty).exists('element')) return 'xml';
+			if (ty.indexOf('element') > -1) return 'xml';
 			return ty;
 		},
 		clean: function(a){
