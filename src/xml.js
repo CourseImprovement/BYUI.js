bef681ce9306ed587d605f442a6c31600236ea97
= <chase.willden@gmail.com>
2015-11-23 13:46:27 -0700

    updated val function

--- src/xml.js
+++ src/xml.js
@@ -29,6 +29,10 @@ byui.extend('createNode', function(name, obj){
 	return $(xml)[0];
 });
 
+byui.fn('createNode', function(name, obj){
+	this.context = byui.createNode(name, obj);
+});
+
 byui.extend('registerXmlTemplate', function(obj){
 	if (byui.fn._internal.getType(obj.obj) != 'object') throw 'Expected object';
 	var keys = Object.keys(obj.obj);
